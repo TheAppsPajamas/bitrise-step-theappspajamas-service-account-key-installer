dbc22c5cc132bb951e8237e87fcac04a43250581
Dean Marcussen <dean@theappspajamas.com>
2019-03-02 14:10:07 +0000

    added test output

--- step.sh
+++ step.sh
@@ -7,5 +7,9 @@ if [ -z "${service_account_key}" ] ; then
   exit 1
 fi
 
-printf "{\n  \x22ServiceUserAccessKey\x22: \x22${service_account_key}\x22\n}" > ${BITRISE_SOURCE_DIR}/tapsecurity.json
 
+CONFIG_tap_security_path="${bitrise_source_dir}/tapsecurity.json"
+
+printf "{\n  \x22ServiceUserAccessKey\x22: \x22${service_account_key}\x22\n}" > CONFIG_tap_security_path
+
+envman add --key "TAP_SECURITY_PATH" --value "${CONFIG_tap_security_path}"
